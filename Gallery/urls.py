ce333b002d1c0d859febf35ec1cad3d4d4abd3fb
DeivisonSantos <deivisonddj3@gmail.com>
2023-12-24 11:52:42 -0300

    Pagina Album

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery_posts, name='gallery_posts'),
+    path('album//', views.view_album, name='view_album'),
 ]
--- Gallery/views.py
+++ Gallery/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Img_Files
 
 def gallery_posts(request):
     All_Posts = Post.objects.all()
     return render(request, 'index.html', {'Posts': All_Posts})
 
-# def get_img():
-#     All_Post = Post.query.all()
-#     return All_Post
+def view_album(request, pk):
+    album = get_object_or_404(Post, pk=pk)
+    return render(request, 'album.html', {'album':album} )
