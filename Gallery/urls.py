493cb0534c82ca9df194a296fe89c0158eccfb7f
DeivisonSantos <deivisonddj3@gmail.com>
2024-01-02 11:27:46 -0300

    Página de Adicionar Posts

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery_posts, name='gallery_posts'),
-    path('album/', views.view_album, name='view_album'),
+    path('/album/', views.view_album, name='view_album'),
+    path('Gallery/New_Album', views.new_album, name='new_album'),
 ]
--- Gallery/views.py
+++ Gallery/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Img_Files
+from django.utils import timezone
 
 def gallery_posts(request):
     All_Posts = Post.objects.all()
     return render(request, 'index.html', {'Posts': All_Posts})
 
+
 def view_album(request, pk):
     album = get_object_or_404(Post, pk=pk)
+
     return render(request, 'album.html', {'album':album} )
+
+
+def new_album(request):
+    if request.method == "POST":
+        new_post = Post.objects.create(
+            author = request.POST['author'],
+            title = request.POST['title'],
+            text = request.POST['text'],
+            created_date = timezone.now()
+        )
+
+        imagens = request.FILES.getlist('imgs')
+        for img in imagens:
+            img_file = Img_Files.objects.create(img=img)
+            new_post.posted_imgs.add(img_file)
+        
+        new_post.save()
+
+        return redirect('view_album', pk=new_post.pk)
+    
+    else:
+        return render (request, 'new_album.html')
